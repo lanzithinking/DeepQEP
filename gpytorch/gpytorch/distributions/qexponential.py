2b6f0b013ec5e496df95886f2a404e1ce2e55acc
SHIWEI LAN <shiweilan@SHIWEIs-iMac.local>
2024-08-31 11:03:38 -0700

    reverse previous change

--- gpytorch/gpytorch/distributions/qexponential.py
+++ gpytorch/gpytorch/distributions/qexponential.py
@@ -57,8 +57,8 @@ class QExponential(ExponentialFamily, Distribution):
             batch_shape = torch.Size()
         else:
             batch_shape = self.loc.size()
-        super().__init__(batch_shape, validate_args=validate_args)
         self.power = power
+        super().__init__(batch_shape, validate_args=validate_args)
 
     def expand(self, batch_shape, _instance=None):
         new = self._get_checked_instance(QExponential, _instance)
